e256c963dc4e9d554adf7b79f6eae74268b17152
tanaka <tanaka.mawoyo@yahoo.com>
2022-11-21 13:13:54 +0100

    Conforming to the MVC design Pattern

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -14,25 +14,35 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber : Bool = true
     
+    private var displayValue: Double {
+        
+        get {
+            guard let number = Double(displayLabel.text!) else {fatalError (" Cannot convert display label text to a Double ")}
+            
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+        
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {fatalError (" Cannot convert display label text to a Double ")}
-        
         if let calcMethod = sender.currentTitle {
-            if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
-            }else if calcMethod == "%"{
-                displayLabel.text = String(number / 100)
-            }else if calcMethod == "AC"{
-                displayLabel.text = "0"
-            }
+            
+            let calculator = CalculatorLogic(number: displayValue)
+            
+            guard let result = calculator.calculate(symbol: calcMethod) else { fatalError("The result of calculation is nil")}
+                
+            displayValue = result
+            
         }
         
     }
-    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
@@ -44,10 +54,8 @@ class ViewController: UIViewController {
             } else {
                 
                 if numValue == "." {
-                    guard let currentDisplayValue = Double(displayLabel.text!) else { fatalError(" Cannot conver display label text to Double")
-                    }
                     
-                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    let isInt = floor(displayValue) == displayValue
                     
                     if !isInt {
                         return
